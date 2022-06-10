3f0569c3cc514623cb1976522d6d79ca6b851555
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-10 10:13:29 +0300

    task 10

---
+++ Task_010/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+// ф-я нахождения второй цифры
+// возвращает вторую цифру переданного числа или
+// ошибку если число не 3х значное: 
+//          -1 - число из 2х знаков или менее
+//          -2 - число из 4х знаков или более
+int CalcNumber(int n){
+    
+    // приведем к положительному
+    if(n < 0) n = -n;
+
+    // проверим условия
+    if(n < 100) return -1;
+
+    if(n > 999) return -2;
+
+    // вычислим результат
+    return (n%100)/10;
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = CalcNumber(num);
+
+if(result >= 0){
+    Console.WriteLine($"Second digit: {result}");
+}
+else if(result == -1){
+    Console.WriteLine("Error! Number has two or less digits.");
+}
+else{
+    Console.WriteLine("Error! Number has four or more digits.");
+}
+
+
+
+
+
+
