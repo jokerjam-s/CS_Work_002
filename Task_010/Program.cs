5cf68d992f86c03c23214b54c044585e11157632
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-10 19:46:12 +0300

    Task 13 start

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -9,7 +9,15 @@
 
 */
 
-// ф-я нахождения второй цифры
+
+// метод для запроса числа
+int InputNum(){
+    Console.Write("Input number: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+// метод нахождения второй цифры трехзначного числа
 // возвращает вторую цифру переданного числа или
 // ошибку если число не 3х значное: 
 //          -1 - число из 2х знаков или менее
@@ -25,11 +33,11 @@ int CalcNumber(int n){
     if(n > 999) return -2;
 
     // вычислим результат
-    return (n%100)/10;
+    return n % 100 / 10;
 }
 
-Console.Write("Input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+
+int num = InputNum();
 int result = CalcNumber(num);
 
 if(result >= 0){
